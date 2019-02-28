1af591bec1713e6e94ef972b0fc08b5b87fd4e10
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-28 08:49:21 -0600

    New method to assing dynamic varaible

---
+++ variableDefining/script.groovy
@@ -0,0 +1,11 @@
+
+
+def objects = [:]
+
+objects['name'] = 'Farkhod'
+
+def isFarkhod = (objects['name'] == 'Farkhod') ? true : false
+def isAbdul = (objects['name'] == 'Abdul') ? true : false
+
+println(isFarkhod)
+println(isAbdul)

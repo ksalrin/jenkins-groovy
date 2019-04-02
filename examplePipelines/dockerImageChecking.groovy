c2d473af1c34158440c18114f548a8ddc7d7c8d8
Farkhod <sadykovfarkhod@gmail.com>
2019-04-02 10:32:58 -0500

    Added example for docker image checking

---
+++ examplePipelines/dockerImageChecking.groovy
@@ -0,0 +1,22 @@
+import groovy.json.JsonSlurper
+
+def mapedData = [:]
+
+node {
+
+  def version = '5.0.4'
+  def newVersion = 0.8
+  def mapdata.data = 'Example Data'
+  stage('Check') {
+    def jsonReader = new JsonSlurper()
+    def dataFromUrl = jsonReader.parse( new URL('http://nexus.fscoding.com/service/rest/v1/components?repository=fscoding') )
+    def versions = dataFromUrl.items.version
+
+    if (dataFromUrl.items.version.contains(newVersion)) {
+      println('I am not going to deploy')
+    } else {
+      println("Shared variable ${version}")
+      println("Integer sharing ${newVersion}")
+    }
+  }
+}

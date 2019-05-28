db46710670b4fcb70cb691353c449d9f5e5f6ab4
Farkhod <sadykovfarkhod@gmail.com>
2019-05-28 15:50:10 -0500

    Checking with new method

--- JenkinsJobs/getCredsId.groovy
+++ JenkinsJobs/getCredsId.groovy
@@ -2,9 +2,13 @@
 
 def credId  = scm.getUserRemoteConfigs()[0].getCredentialsId()
 
-def repo = 'https://github.com/fsadykov/keep-creds.git'.replace('https://', '')
+// def repo = 'https://github.com/fsadykov/keep-creds.git'.replace('https://', '')
+
+
+String repoUrl = project.getDefinition().getScm().getUserRemoteConfigs()[0].getUrl()
 
 node('master') {
+
   echo "${credId}"
   withCredentials([[$class: 'UsernamePasswordMultiBinding',
   credentialsId: "${credId}",
@@ -12,7 +16,7 @@ node('master') {
   passwordVariable: 'GIT_PASSWORD']]) {
     dir("${WORKSPACE}/") {
       sh 'rm -rf keep-creds'
-      sh("git clone https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${repo} ")
+      sh("git clone https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${repoUrl} ")
       sh "ls -la "
     }
   }

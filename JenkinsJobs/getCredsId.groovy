d6de0891ceacbb7ef8a394b60698a8e65a801408
Farkhod <sadykovfarkhod@gmail.com>
2019-05-29 12:04:58 -0500

    Checking build date

--- JenkinsJobs/getCredsId.groovy
+++ JenkinsJobs/getCredsId.groovy
@@ -7,13 +7,12 @@ node('master') {
   checkout scm
 
   echo "${credId}"
-  withCredentials([[$class: 'UsernamePasswordMultiBinding',
-  credentialsId: "${credId}",
-  usernameVariable: 'GIT_USERNAME',
-  passwordVariable: 'GIT_PASSWORD']]) {
+  withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credId}", usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD']]) {
     dir("${WORKSPACE}/") {
       sh("git tag -a some_tag -m 'Jenkins'")
       sh("git push https://${env.GIT_USERNAME}:${env.GIT_PASSWORD}@${repoUrl} --tags")
     }
   }
+
+  println("${BUILD_DATE}")
 }

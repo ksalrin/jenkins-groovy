45f69e8e9985069a1153afbd52c03a8bb761d62a
Farkhod <sadykovfarkhod@gmail.com>
2019-08-15 16:45:09 -0500

    Updated for new changes

--- statements/findMarketName.groovy
+++ statements/findMarketName.groovy
@@ -1,8 +1,20 @@
+import java.net.URI;
+import java.text.SimpleDateFormat
 def params = [:]
 
-params['ConfigFile'] = 'dev/us-east-1/dev01-us.tf'
+String dateTime   = new SimpleDateFormat("yyyy/MM/dd.HH-mm-ss").format(Calendar.getInstance().getTime())
+def BUILD_NUMBER = 2
+def deployedToEnv = 'dev'
+params['ConfigFile'] = 'qa/us-east-1/ipe16-us.tf'
+
+
+tagForGit = "deploy_${deployedToEnv}_${BUILD_NUMBER}_${dateTime}"
 
 if (params['ConfigFile'].split('/')[-1].contains('-')) {
-  def market = params['ConfigFile'].split('/')[-1].split('-')[-1].replace('.tf', '')
-  println(market)
+  def market = params['ConfigFile'].split('/')[-1].split('-')[-1].split("\\.")[0];
+  tagForGit = "deploy_${deployedToEnv}_${market}_${BUILD_NUMBER}_${dateTime}"
 }
+
+
+
+println(tagForGit)

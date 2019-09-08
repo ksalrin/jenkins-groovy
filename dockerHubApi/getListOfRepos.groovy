578cfaa77ae8bd33f90340c6098719da6de5f685
Farkhod <sadykovfarkhod@gmail.com>
2019-09-08 18:26:36 -0500

    Created Script to pull docker hub images

---
+++ dockerHubApi/getListOfRepos.groovy
@@ -0,0 +1,30 @@
+import groovy.json.JsonSlurper
+
+
+def findDockerImages(nameApp) {
+  def foundRepo    = ""
+  def token        = ""
+  def myJsonreader = new JsonSlurper()
+  def nexusData = myJsonreader.parse(new URL("https://registry.hub.docker.com/v2/repositories/fuchicorp/"))
+  def repoVersions = []
+
+  nexusData.results.each {
+    if (it.name ==  nameApp ) {
+        foundRepo = it.name
+      }
+    }
+
+  nexusData = myJsonreader.parse(new URL("https://registry.hub.docker.com/v2/repositories/fuchicorp/${foundRepo}/tags/"))
+
+  nexusData.results.each {
+    repoVersions.add( "${foundRepo}:" +  it.name)
+  }
+
+  if(!repoVersions) {
+    versionList.add('ImmageNotFound')
+  }
+
+  return repoVersions.sort()
+}
+
+println(findDockerImages('cloud'))
